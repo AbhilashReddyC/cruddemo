01caf15c359e8ea00de3c97fa54143a432d52eb6
Abhilash <chadaabhilash@gmail.com>
2019-04-03 14:22:46 -0500

    /Users/abhi/git/cruddemo/cruddemo/src/main/java/com/luv2code/springboot/cruddemo/CruddemoApplication.java

--- cruddemo/src/main/java/com/luv2code/springboot/cruddemo/CruddemoApplication.java
+++ cruddemo/src/main/java/com/luv2code/springboot/cruddemo/CruddemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CruddemoApplication {
-
+//main method to run APP
 	public static void main(String[] args) {
 		SpringApplication.run(CruddemoApplication.class, args);
 	}
